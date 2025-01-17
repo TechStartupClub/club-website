9406c8512fca17085473654eee9726a0f914f40e
celestinryf <celestinryf@gmail.com>
2025-01-17 10:24:27 -0800

    feat: demo video for UMarket project page

--- src/routes/UMarket.js
+++ src/routes/UMarket.js
@@ -1,15 +1,24 @@
 import React from 'react';
-// import style from './style/joinus.module.scss';
+import style from './style/umarket.module.scss';
 import NavBar from '../components/Navbar';
 import Footer from '../components/Footer';
 // import { GiUrsaMajor } from 'react-icons/gi';
-
+import UMarketVideo from '../assets/images/UMarket/videos/UMarket demo.mov';
 
 const UMarket = () => {
     return (
         <>
+            <div className={style.layoutWrapper}>
             <NavBar />
+            <div className={style.mainContent}>
+                <div className={style.videoBox}>
+                    <video autoPlay muted loop playsInline>
+                        <source src={UMarketVideo} type="video/mp4" />
+                    </video>
+                </div>
+            </div>
             <Footer />
+            </div>
         </>
     )
 }
